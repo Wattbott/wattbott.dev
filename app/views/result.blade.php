e5d6995eed7afcfdcd28d9226ad215be214567ed
Joseph Acevedo <joseph.acevedo.mail@gmail.com>
2016-05-05 11:58:32 -0500

    Some more work on the results page.

--- app/views/result.blade.php
+++ app/views/result.blade.php
@@ -1,7 +1,3 @@
-<?php 	
-
-?>
-
 @extends('layouts.master')
 @section('style')
 <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,900' rel='stylesheet' type='text/css'>
@@ -11,9 +7,10 @@
 
 <div class="mainresultscontainer">
 	<div id="resultstext">Alright the results are in for {{{ $run->run['user_input']['run_name']}}}!</div>
+	<div id="superselect"></div>
 	<div id="graph0" class="dagraph">
-		<span class="graphlabel">EUI</span>
-		<div id="bar1" class="graphbar">
+		<span  class="graphlabel">EUI</span>
+		<div id="bar1" class="graphbar yourbar">
 			<span class="bartext">{{{ $run->run['user_output']['eui']['design_site_intensity'] }}}</span>
 		</div>
 		<div id="bar3" class="graphbar theirbar">
@@ -22,7 +19,7 @@
 	</div>
 	<div id="graph1" class="dagraph">
 		<span class="graphlabel">Energy Cost</span>
-		<div id="bar2" class="graphbar">
+		<div id="bar2" class="graphbar yourbar">
 			<span class="bartext">{{{ $run->run['user_output']['eui']['design_energy_cost'] }}}</span>
 		</div>
 		<div id="bar4" class="graphbar theirbar">
@@ -37,6 +34,34 @@
 		<div id="bar6" class="graphbar">
 			<span class="bartext">{{{ $run->run['user_output']['pv']['percent_savings'] }}}
 			</span>
+		</div>
+	</div>
+	<div id="kewlkontainer">
+		<div class="infocontainer">
+			<p class="infoheader fontmidlarge">Energy Use Intensity</p>
+			<p class="kewlkontents fontmid">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+		<div class="infocontainer">
+			<p class="infoheader fontmidlarge">Energy Cost</p>
+			<p class="kewlkontents fontmid">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+		<div class="infocontainer">
+			<p class="infoheader fontmidlarge">Photovoltaic</p>
+			<p class="kewlkontents fontmid">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</div>
 	</div>
 </div>
