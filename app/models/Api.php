768b7af062482f95cae8f5cdf4b1639a64806bc8
Anna Morton <annajmorton@gmail.com>
2016-05-06 10:34:19 -0500

    more bug fixes

--- app/models/Api.php
+++ app/models/Api.php
@@ -3,7 +3,7 @@
 class Api extends Eloquent {
 
     static public $url_pv = "http://api.data.gov/nrel/pvwatts/v5.json?";
-    static public $url_geo = "http://api.data.gov/nrel/pvwatts/v5.json?";
+    static public $url_eui = "https://portfoliomanager.energystar.gov/ws/targetFinder?measurementSystem=EPA";
 
     public static function setLocation($zipcode)
     {
@@ -34,13 +34,13 @@ class Api extends Eloquent {
 		];
 		$url_pv = static::$url_pv . http_build_query($query);
 
+    	// set variables for curl http request
     	$options = [
     		CURLOPT_URL => $url_pv,
     		CURLOPT_FAILONERROR => true,
 			CURLINFO_HEADER_OUT => true,
 			CURLOPT_RETURNTRANSFER => true,
     	];
-		
     	curl_setopt_array($ch, $options);
 
 		//execute post
@@ -54,13 +54,45 @@ class Api extends Eloquent {
 		$result = json_decode($result,true);
 		return $result['outputs']['ac_annual'];
     }
+
     public function eui()
     {
+    	// build xml request with object
+    	$filename = public_path() . "/xml/targetfinder.xml";
+		$query = simplexml_load_file($filename);
+		dd($query);
+		$xml_query = $query->asXML();
+		
+		// set variables for curl http request
+		$ch = curl_init();
+		$options = [
+    		CURLOPT_URL => static::$url_eui,
+    		CURLOPT_FAILONERROR => true,
+			CURLINFO_HEADER_OUT => false,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_POST => true,
+			CURLOPT_POSTFIELDS =>  $xml_query,
+			CURLOPT_HTTPHEADER => ['Content-Type: application/xml'],
+			CURLOPT_HTTP200ALIASES => [-200,-100],
+    	];
+    	curl_setopt_array($ch, $options);
+		
+		// catch response and convert it to object
+		$xml_response = curl_exec($ch);
+		$response = simplexml_load_string($xml_response);
+
+		// info and error catching on response
+		$info = curl_getinfo($ch);
+		$error = curl_error($ch);
+
+		//close connection
+		curl_close($ch);
+
     	$result = [
-			'design_site_intensity' => 0,
-			'design_energy_cost' => 0,
-			'median_site_intensity' => 0,
-			'median_energy_cost' => 0
+			'design_site_intensity' => $response->metric[11],
+			'design_energy_cost' => $response->metric[17],
+			'median_site_intensity' => $response->metric[1],
+			'median_energy_cost' => $response->metric[7],
 		];
 	
 		return $result;
