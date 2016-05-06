42a9874922c4d982d3f80e78bb27131ba440c62b
Joseph Acevedo <joseph.acevedo.mail@gmail.com>
2016-05-06 13:46:01 -0500

    Dancing trees

---
+++ app/views/hello.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.master')
+
+@section('style')
+<link href='https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,900' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/css/hello.css">
+@stop
+
+@section('content')
+<h1 id="title" class="font3">WATTBOTT</h1>
+<div class="falsemargin"></div>
+<div id="magicplace">
+	<div id="ground" class="circle"></div>
+	<div id="tree1" class="tree">
+		<div class="happyface">
+			<div class="eye eyeone"></div>
+			<div class="eye eyetwo"></div>
+			<div class="smiley"><div class="smileyoverlay"></div></div>
+		</div>
+		<div class="treefoliage1">
+			<div class="treefoliage1part1"><div class="treefoliage1part2"></div><div class="treefoliage1part3"></div></div>
+		</div>
+		<div class="treetrunk"></div>
+	</div>
+	<div id="tree2" class="tree">
+		<div class="happyface">
+			<div class="eye eyeone"></div>
+			<div class="eye eyetwo"></div>
+			<div class="smiley"><div class="smileyoverlay"></div></div>
+		</div>
+		<div class="treefoliage2">
+			<div class="treefoliage2part1 circle"></div><div class="treefoliage2part2 circle"></div><div class="treefoliage2part3 circle"></div><div class="treefoliage2part4 circle"></div><div class="treefoliage2part5 circle"></div>
+		</div>
+		<div class="treetrunk"></div>
+	</div>
+	<div class="sun">
+		<div class="happyface">
+				<div class="eye eyeone"></div>
+				<div class="eye eyetwo"></div>
+				<div class="smiley"><div class="smileyoverlay"></div></div>
+			</div>
+		<div class="suncorona"><div class="coronapart lazycorona1"></div><div class="coronapart lazycorona2"></div></div>
+		<div class="sunbody">
+			
+		</div>
+	</div>
+	<div id="school" class="building">
+		<div id="schoolbody" class="building">
+			<div id="schwindow1" class="window">
+				<div class="windowhorpane"></div>
+				<div class="windowvertpane"></div>
+				<div class="windowglare">
+					<div class="innerglare"></div>
+				</div>
+			</div>
+			<div id="schwindow2" class="window">
+				<div class="windowhorpane"></div>
+				<div class="windowvertpane"></div>
+				<div class="windowglare">
+					<div class="innerglare"></div>
+				</div>
+			</div>
+			<div id="schdoor" class="door building">
+				<div class="knob circle"></div>
+			</div>
+		</div>
+		<div id="schoolroof" class="building"><div id="schooltop"></div></div>
+	</div>
+</div>
+<a href="{{{action('HomeController@testForm')}}}"><div class="buttonlink font3"><p>Start Your Journey Here</p></div></a>
+@stop
+
+@section('scripts')
+<script type="text/javascript" src="/js/hello.js"></script>
+@stop
