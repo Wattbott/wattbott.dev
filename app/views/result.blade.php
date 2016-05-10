015f13b2dd4c9bce499ffde77a6dbab3de8be690
Margot McMahon <amcmahon@nes.ru>
2016-05-10 08:48:21 -0500

    units added to result.blade, lorem ipsum replaced

--- app/views/result.blade.php
+++ app/views/result.blade.php
@@ -17,57 +17,44 @@
 	<div id="graph0" class="dagraph">
 		<span  class="graphlabel">EUI</span>
 		<div id="bar1" class="graphbar yourbar">
-			<span class="bartext">{{{ $run->run['user_output']['eui']['design_site_intensity'] }}}</span>
+			<span class="bartext">{{{ $run->run['user_output']['eui']['design_site_intensity'] }}}</span> kBtu/ft<sup>2</sup>
 		</div>
 		<div id="bar3" class="graphbar theirbar">
-			<span class="bartext">{{{ $run->run['user_output']['eui']['median_site_intensity'] }}}</span>
+			<span class="bartext">{{{ $run->run['user_output']['eui']['median_site_intensity'] }}}</span> kBtu/ft<sup>2</sup>
 		</div>
 	</div>
 	<div id="graph1" class="dagraph">
 		<span class="graphlabel">Energy Cost</span>
 		<div id="bar2" class="graphbar yourbar">
-			<span class="bartext">{{{ $run->run['user_output']['eui']['design_energy_cost'] }}}</span>
+			$<span class="bartext">{{{ $run->run['user_output']['eui']['design_energy_cost'] }}}</span>
 		</div>
 		<div id="bar4" class="graphbar theirbar">
-			<span class="bartext">{{{ $run->run['user_output']['eui']['median_energy_cost'] }}}</span>
+			$<span class="bartext">{{{ $run->run['user_output']['eui']['median_energy_cost'] }}}</span>
 		</div>
 	</div>
 	<div id="graph2" class="dagraph">
 		<span class="graphlabel">Savings from Photovoltaic</span>
 		<div id="bar5" class="graphbar">
-			<span class="bartext">{{{ $run->run['user_output']['pv']['roi'] }}}</span>
+			<span class="bartext">{{{ round($run->run['user_output']['pv']['roi'], 1) }}}</span> years
 		</div>
 		<div id="bar6" class="graphbar">
-			<span class="bartext">{{{ $run->run['user_output']['pv']['percent_savings'] }}}
+			<span class="bartext">{{{ round($run->run['user_output']['pv']['percent_savings'], 1) }}}% annual savings
 			</span>
 		</div>
 	</div>
 	<div id="kewlkontainer">
 		<div class="infocontainer">
 			<p class="infoheader fontmidlarge">Energy Use Intensity (EUI)</p>
-			<p class="kewlkontents fontmid">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+			<p class="kewlkontents fontmid">EUI is expressed as energy per square foot per year. It’s calculated by dividing the total energy consumed by the building in one year (measured in kBtu or GJ) by the total gross floor area of the building. Generally, a low EUI signifies good energy performance.
+			</p>
+		</div>
 		<div class="infocontainer">
 			<p class="infoheader fontmidlarge">Energy Cost</p>
-			<p class="kewlkontents fontmid">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p class="kewlkontents fontmid">Here, we compare your annual energy expenses with the median for buildings of your type and size. How do you compare?</p>
 		</div>
 		<div class="infocontainer">
 			<p class="infoheader fontmidlarge">Photovoltaic</p>
-			<p class="kewlkontents fontmid">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p class="kewlkontents fontmid">A photovoltaic system on your building could save you ${{ round($run->run['user_output']['pv']['percent_savings'] * $run->run['user_output']['eui']['design_energy_cost']) }} per year, and pay for itself in {{{ round($run->run['user_output']['pv']['roi'], 1) }}} years.</p>
 		</div>
 	</div>
 	<div class="littlecontainer">
