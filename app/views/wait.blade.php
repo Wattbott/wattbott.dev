dba020d6e20c82a2013a89d0a0d8f58b77511d7d
Margot McMahon <amcmahon@nes.ru>
2016-05-10 13:24:45 -0500

    wait view filled out with CRAP

---
+++ app/views/wait.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('style')
+<link href='https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,900' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/css/hello.css">
+@stop
+
+@section('content')
+<h1 id="errorTitle" class="font3">CALCULATING</h1>
+@stop
